6527d75d8513c96acfb46e588997b1cdba56fef8
BauThuan <bauthuanxd@gmail.com>
2023-08-09 16:08:52 +0700

    Ok

---
+++ src/migrations/migration-create-book.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Book", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.BLOB("medium"),
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      purchases: {
+        type: Sequelize.INTEGER,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      percent: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Book");
+  },
+};
